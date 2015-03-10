d7fbd5b3157411c7a61e5eed0b9022f9bd138345
Joshua Harlow <harlowja@yahoo-inc.com>
2015-03-10 11:06:40 -0700

    Remove py33 support
    Just mention/retain py34 support instead.

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
     ],
     keywords="locks thread threads",
