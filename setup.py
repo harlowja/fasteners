f842cd6cb2216553c7c859d823cfcda6da577da6
Joshua Harlow <harlowja@gmail.com>
2015-11-12 22:42:36 -0800

    0.14.0 version update

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ install_requires = [
 
 setup(
     name='fasteners',
-    version='0.13.0',
+    version='0.14.0',
     description='A python package that provides useful locks.',
     author="Joshua Harlow",
     author_email='harlowja@yahoo-inc.com',
