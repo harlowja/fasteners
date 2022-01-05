b75dc75e338fc1af0d8f104938aa5a710e949789
Paulius Sarka <paulius.sarka@gmail.com>
2022-01-05 16:40:44 +0100

    Add python3.10 to the test matrix

--- setup.py
+++ setup.py
@@ -44,6 +44,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: PyPy"
     ],
     python_requires=">=3.6",
